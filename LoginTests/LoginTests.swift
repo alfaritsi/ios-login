1ad7304ebdb553faeefff265a05e7a01a74b7553
el-Barbasy <lnn.hakim@gmail.com>
2026-04-22 08:49:16 +0700

    Initial Commit

---
+++ LoginTests/LoginTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTests.swift
+//  LoginTests
+//
+//  Created by el-Barbasy on 22/04/26.
+//
+
+import Testing
+@testable import Login
+
+struct LoginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
